921d760c7718b00c509db296fa8848d264e812af
tommmooooooo <tomokane.software@gmail.com>
2024-06-09 13:38:59 +0100

    more work on parsing data

--- Tool/frmMain.Designer.vb
+++ Tool/frmMain.Designer.vb
@@ -28,7 +28,24 @@ Partial Class frmMain
         Me.grpBoxSelectProject = New System.Windows.Forms.GroupBox()
         Me.lblRawProjectPath = New System.Windows.Forms.Label()
         Me.lstbxStation1Files = New System.Windows.Forms.ListBox()
+        Me.lstbxRowData = New System.Windows.Forms.ListBox()
+        Me.grpboxMotionRow = New System.Windows.Forms.GroupBox()
+        Me.btnAdv = New System.Windows.Forms.Button()
+        Me.btnRet = New System.Windows.Forms.Button()
+        Me.lblMotionNameSym = New System.Windows.Forms.Label()
+        Me.lblMotionNameAbs = New System.Windows.Forms.Label()
+        Me.lblReturnDepthSym = New System.Windows.Forms.Label()
+        Me.lblReturnCoilSym = New System.Windows.Forms.Label()
+        Me.lblReturnDepthAbs = New System.Windows.Forms.Label()
+        Me.lblReturnCoilAbs = New System.Windows.Forms.Label()
+        Me.lblAdvanceDepthSym = New System.Windows.Forms.Label()
+        Me.lblAdvanceCoilSym = New System.Windows.Forms.Label()
+        Me.lblAdvanceDepthAbs = New System.Windows.Forms.Label()
+        Me.lblAdvanceCoilAbs = New System.Windows.Forms.Label()
+        Me.lblStnNumber = New System.Windows.Forms.Label()
+        Me.lblRowNumber = New System.Windows.Forms.Label()
         Me.grpBoxSelectProject.SuspendLayout()
+        Me.grpboxMotionRow.SuspendLayout()
         Me.SuspendLayout()
         '
         'btnOpenProject
@@ -83,20 +100,195 @@ Partial Class frmMain
         Me.lstbxStation1Files.Location = New System.Drawing.Point(81, 162)
         Me.lstbxStation1Files.Name = "lstbxStation1Files"
         Me.lstbxStation1Files.ScrollAlwaysVisible = True
-        Me.lstbxStation1Files.Size = New System.Drawing.Size(576, 95)
+        Me.lstbxStation1Files.Size = New System.Drawing.Size(576, 56)
         Me.lstbxStation1Files.TabIndex = 4
+        '
+        'lstbxRowData
+        '
+        Me.lstbxRowData.FormattingEnabled = True
+        Me.lstbxRowData.Location = New System.Drawing.Point(81, 240)
+        Me.lstbxRowData.Name = "lstbxRowData"
+        Me.lstbxRowData.ScrollAlwaysVisible = True
+        Me.lstbxRowData.Size = New System.Drawing.Size(576, 56)
+        Me.lstbxRowData.TabIndex = 5
+        '
+        'grpboxMotionRow
+        '
+        Me.grpboxMotionRow.Controls.Add(Me.lblRowNumber)
+        Me.grpboxMotionRow.Controls.Add(Me.lblStnNumber)
+        Me.grpboxMotionRow.Controls.Add(Me.btnAdv)
+        Me.grpboxMotionRow.Controls.Add(Me.btnRet)
+        Me.grpboxMotionRow.Controls.Add(Me.lblMotionNameSym)
+        Me.grpboxMotionRow.Controls.Add(Me.lblMotionNameAbs)
+        Me.grpboxMotionRow.Controls.Add(Me.lblReturnDepthSym)
+        Me.grpboxMotionRow.Controls.Add(Me.lblReturnCoilSym)
+        Me.grpboxMotionRow.Controls.Add(Me.lblReturnDepthAbs)
+        Me.grpboxMotionRow.Controls.Add(Me.lblReturnCoilAbs)
+        Me.grpboxMotionRow.Controls.Add(Me.lblAdvanceDepthSym)
+        Me.grpboxMotionRow.Controls.Add(Me.lblAdvanceCoilSym)
+        Me.grpboxMotionRow.Controls.Add(Me.lblAdvanceDepthAbs)
+        Me.grpboxMotionRow.Controls.Add(Me.lblAdvanceCoilAbs)
+        Me.grpboxMotionRow.Location = New System.Drawing.Point(53, 328)
+        Me.grpboxMotionRow.Name = "grpboxMotionRow"
+        Me.grpboxMotionRow.Size = New System.Drawing.Size(672, 160)
+        Me.grpboxMotionRow.TabIndex = 6
+        Me.grpboxMotionRow.TabStop = False
+        Me.grpboxMotionRow.Text = "Motion Row Sample"
+        '
+        'btnAdv
+        '
+        Me.btnAdv.Location = New System.Drawing.Point(6, 19)
+        Me.btnAdv.Name = "btnAdv"
+        Me.btnAdv.Size = New System.Drawing.Size(58, 134)
+        Me.btnAdv.TabIndex = 11
+        Me.btnAdv.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        Me.btnAdv.UseVisualStyleBackColor = True
+        '
+        'btnRet
+        '
+        Me.btnRet.Location = New System.Drawing.Point(598, 20)
+        Me.btnRet.Name = "btnRet"
+        Me.btnRet.Size = New System.Drawing.Size(58, 134)
+        Me.btnRet.TabIndex = 10
+        Me.btnRet.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        Me.btnRet.UseVisualStyleBackColor = True
+        '
+        'lblMotionNameSym
+        '
+        Me.lblMotionNameSym.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblMotionNameSym.Location = New System.Drawing.Point(246, 56)
+        Me.lblMotionNameSym.Name = "lblMotionNameSym"
+        Me.lblMotionNameSym.Size = New System.Drawing.Size(165, 27)
+        Me.lblMotionNameSym.TabIndex = 9
+        Me.lblMotionNameSym.Text = "Motion Name Sym"
+        Me.lblMotionNameSym.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'lblMotionNameAbs
+        '
+        Me.lblMotionNameAbs.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblMotionNameAbs.Location = New System.Drawing.Point(246, 20)
+        Me.lblMotionNameAbs.Name = "lblMotionNameAbs"
+        Me.lblMotionNameAbs.Size = New System.Drawing.Size(165, 27)
+        Me.lblMotionNameAbs.TabIndex = 8
+        Me.lblMotionNameAbs.Text = "Motion Name abs"
+        Me.lblMotionNameAbs.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'lblReturnDepthSym
+        '
+        Me.lblReturnDepthSym.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblReturnDepthSym.Location = New System.Drawing.Point(417, 130)
+        Me.lblReturnDepthSym.Name = "lblReturnDepthSym"
+        Me.lblReturnDepthSym.Size = New System.Drawing.Size(165, 27)
+        Me.lblReturnDepthSym.TabIndex = 7
+        Me.lblReturnDepthSym.Text = "Return Depth Sym"
+        Me.lblReturnDepthSym.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        '
+        'lblReturnCoilSym
+        '
+        Me.lblReturnCoilSym.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblReturnCoilSym.Location = New System.Drawing.Point(417, 94)
+        Me.lblReturnCoilSym.Name = "lblReturnCoilSym"
+        Me.lblReturnCoilSym.Size = New System.Drawing.Size(165, 27)
+        Me.lblReturnCoilSym.TabIndex = 6
+        Me.lblReturnCoilSym.Text = "Return Coil Sym"
+        Me.lblReturnCoilSym.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        Me.lblReturnCoilSym.UseMnemonic = False
+        '
+        'lblReturnDepthAbs
+        '
+        Me.lblReturnDepthAbs.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblReturnDepthAbs.Location = New System.Drawing.Point(417, 56)
+        Me.lblReturnDepthAbs.Name = "lblReturnDepthAbs"
+        Me.lblReturnDepthAbs.Size = New System.Drawing.Size(165, 27)
+        Me.lblReturnDepthAbs.TabIndex = 5
+        Me.lblReturnDepthAbs.Text = "Return Depth abs"
+        Me.lblReturnDepthAbs.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        '
+        'lblReturnCoilAbs
+        '
+        Me.lblReturnCoilAbs.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblReturnCoilAbs.Location = New System.Drawing.Point(417, 20)
+        Me.lblReturnCoilAbs.Name = "lblReturnCoilAbs"
+        Me.lblReturnCoilAbs.Size = New System.Drawing.Size(165, 27)
+        Me.lblReturnCoilAbs.TabIndex = 4
+        Me.lblReturnCoilAbs.Text = "Return Coil abs"
+        Me.lblReturnCoilAbs.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        '
+        'lblAdvanceDepthSym
+        '
+        Me.lblAdvanceDepthSym.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblAdvanceDepthSym.Location = New System.Drawing.Point(70, 130)
+        Me.lblAdvanceDepthSym.Name = "lblAdvanceDepthSym"
+        Me.lblAdvanceDepthSym.Size = New System.Drawing.Size(165, 27)
+        Me.lblAdvanceDepthSym.TabIndex = 3
+        Me.lblAdvanceDepthSym.Text = "Advance Depth Sym"
+        Me.lblAdvanceDepthSym.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        '
+        'lblAdvanceCoilSym
+        '
+        Me.lblAdvanceCoilSym.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblAdvanceCoilSym.Location = New System.Drawing.Point(70, 94)
+        Me.lblAdvanceCoilSym.Name = "lblAdvanceCoilSym"
+        Me.lblAdvanceCoilSym.Size = New System.Drawing.Size(165, 27)
+        Me.lblAdvanceCoilSym.TabIndex = 2
+        Me.lblAdvanceCoilSym.Text = "Advance Coil Sym"
+        Me.lblAdvanceCoilSym.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.lblAdvanceCoilSym.UseMnemonic = False
+        '
+        'lblAdvanceDepthAbs
+        '
+        Me.lblAdvanceDepthAbs.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblAdvanceDepthAbs.Location = New System.Drawing.Point(70, 56)
+        Me.lblAdvanceDepthAbs.Name = "lblAdvanceDepthAbs"
+        Me.lblAdvanceDepthAbs.Size = New System.Drawing.Size(165, 27)
+        Me.lblAdvanceDepthAbs.TabIndex = 1
+        Me.lblAdvanceDepthAbs.Text = "Advance Depth abs"
+        Me.lblAdvanceDepthAbs.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        '
+        'lblAdvanceCoilAbs
+        '
+        Me.lblAdvanceCoilAbs.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblAdvanceCoilAbs.Location = New System.Drawing.Point(70, 20)
+        Me.lblAdvanceCoilAbs.Name = "lblAdvanceCoilAbs"
+        Me.lblAdvanceCoilAbs.Size = New System.Drawing.Size(165, 27)
+        Me.lblAdvanceCoilAbs.TabIndex = 0
+        Me.lblAdvanceCoilAbs.Text = "Advance Coil abs"
+        Me.lblAdvanceCoilAbs.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        '
+        'lblStnNumber
+        '
+        Me.lblStnNumber.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblStnNumber.Location = New System.Drawing.Point(285, 94)
+        Me.lblStnNumber.Name = "lblStnNumber"
+        Me.lblStnNumber.Size = New System.Drawing.Size(91, 27)
+        Me.lblStnNumber.TabIndex = 12
+        Me.lblStnNumber.Text = "stnNum"
+        Me.lblStnNumber.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'lblRowNumber
+        '
+        Me.lblRowNumber.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblRowNumber.Location = New System.Drawing.Point(285, 126)
+        Me.lblRowNumber.Name = "lblRowNumber"
+        Me.lblRowNumber.Size = New System.Drawing.Size(91, 27)
+        Me.lblRowNumber.TabIndex = 13
+        Me.lblRowNumber.Text = "RowNum"
+        Me.lblRowNumber.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
         '
         'frmMain
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(800, 522)
+        Me.Controls.Add(Me.grpboxMotionRow)
+        Me.Controls.Add(Me.lstbxRowData)
         Me.Controls.Add(Me.lstbxStation1Files)
         Me.Controls.Add(Me.lblRawProjectPath)
         Me.Controls.Add(Me.grpBoxSelectProject)
         Me.Name = "frmMain"
         Me.Text = "TOAST Text Export Tool"
         Me.grpBoxSelectProject.ResumeLayout(False)
+        Me.grpboxMotionRow.ResumeLayout(False)
         Me.ResumeLayout(False)
 
     End Sub
@@ -107,4 +299,20 @@ Partial Class frmMain
     Friend WithEvents grpBoxSelectProject As GroupBox
     Friend WithEvents lblRawProjectPath As Label
     Friend WithEvents lstbxStation1Files As ListBox
+    Friend WithEvents lstbxRowData As ListBox
+    Friend WithEvents grpboxMotionRow As GroupBox
+    Public WithEvents lblAdvanceCoilAbs As Label
+    Public WithEvents lblAdvanceDepthAbs As Label
+    Public WithEvents lblAdvanceDepthSym As Label
+    Public WithEvents lblAdvanceCoilSym As Label
+    Public WithEvents lblMotionNameAbs As Label
+    Public WithEvents lblReturnDepthSym As Label
+    Public WithEvents lblReturnCoilSym As Label
+    Public WithEvents lblReturnDepthAbs As Label
+    Public WithEvents lblReturnCoilAbs As Label
+    Public WithEvents btnAdv As Button
+    Public WithEvents btnRet As Button
+    Public WithEvents lblMotionNameSym As Label
+    Public WithEvents lblStnNumber As Label
+    Public WithEvents lblRowNumber As Label
 End Class
--- Tool/frmMain.vb
+++ Tool/frmMain.vb
@@ -1,8 +1,4 @@
-﻿Imports System.Diagnostics.Eventing
-Imports System.Xml
-Imports System.Xml.Serialization
-
-Public Class frmMain
+﻿Public Class frmMain
     Dim tc3progLocationFiler As String
     Dim RawProjectPath As String
 
@@ -11,6 +7,13 @@ Public Class frmMain
     Dim station1Location As String
 
     Dim OpenedFile
+
+    Dim arryMotionRowText As String()
+
+
+    Const motionheader As String = "&lt;"
+    Const motionfooter As String = "&gt;"
+
 
 
     Private Sub frmMain_Load(sender As Object, e As EventArgs) Handles MyBase.Load
@@ -57,8 +60,6 @@ Public Class frmMain
         OpenedFile = XDocument.Load(SelectedFile)
 
         ''declare values to trim against
-        Dim motionheader As String = "&lt;"
-        Dim motionfooter As String = "&gt;"
         Dim motionheaderLocation As UInt32
         Dim motionfooterLocation As UInt32
 
@@ -67,19 +68,153 @@ Public Class frmMain
         motionheaderLocation = OpenedFile.ToString.IndexOf(motionheader)
 
         ''debug
-        MsgBox("header location is:" & motionheaderLocation)
-        MsgBox("footer location is:" & motionfooterLocation)
+        '' MsgBox("header location is:" & motionheaderLocation)
+        '' MsgBox("footer location is:" & motionfooterLocation)
 
         Dim motiondatastring As String
 
         ''trim file for later use
         motiondatastring = OpenedFile.ToString.Remove(motionfooterLocation + motionfooterLocation.ToString.Length)
         motiondatastring = motiondatastring.ToString.Substring(motionheaderLocation)
-        Console.WriteLine(motiondatastring)
+        ' Console.WriteLine(motiondatastring)
+
+        arryMotionRowText = motiondatastring.Split(vbCr)
+
+
+        lstbxRowData.DataSource = arryMotionRowText
+
+        '&lt;MotionRow&gt; 0
+        '&lt;StationNumber&gt;1&lt;/StationNumber&gt; 1
+        lblStnNumber.Text = ParseRow(lstbxRowData.Items(1).ToString, "StationNumber")
+
+        '&lt;RowNumber&gt;1&lt;/RowNumber&gt; 2
+        lblRowNumber.Text = ParseRow(lstbxRowData.Items(2).ToString, "RowNumber")
+
+        '&lt;Advance&gt; 3
+        '&lt;AdvanceCoilTextSym&gt;Advance&lt;/AdvanceCoilTextSym&gt; 4
+        lblAdvanceCoilSym.Text = ParseRow(lstbxRowData.Items(4).ToString, "AdvanceCoilTextSym")
+
+        '&lt;AdvanceDepthTextSym&gt;Advanced&lt;/AdvanceDepthTextSym&gt; 5
+        lblAdvanceDepthSym.Text = ParseRow(lstbxRowData.Items(5).ToString, "AdvanceDepthTextSym")
+
+        '&lt;AdvanceCoilTextAbs&gt;Q1.0&lt;/AdvanceCoilTextAbs&gt; 6
+        lblAdvanceCoilAbs.Text = ParseRow(lstbxRowData.Items(6).ToString, "AdvanceCoilTextAbs")
+
+        '&lt;AdvanceDepthTextAbs&gt;I1.0&lt;/AdvanceDepthTextAbs&gt; 7
+        lblAdvanceDepthAbs.Text = ParseRow(lstbxRowData.Items(7).ToString, "AdvanceDepthTextAbs")
+
+        '&lt;ManSeq&gt;1&lt;/ManSeq&gt; 8
+        btnAdv.Text = ParseRow(lstbxRowData.Items(8).ToString, "ManSeq")
+        '&lt;/Advance&gt; 9
+        '&lt;Return&gt; 10
+        '&lt;ReturnCoilTextSym&gt;Return&lt;/ReturnCoilTextSym&gt; 11
+        lblReturnCoilSym.Text = ParseRow(lstbxRowData.Items(11).ToString, "ReturnCoilTextSym")
+
+        '&lt;ReturnDepthTextSym&gt;Returned&lt;/ReturnDepthTextSym&gt; 12
+        lblReturnDepthSym.Text = ParseRow(lstbxRowData.Items(12).ToString, "ReturnDepthTextSym")
+
+        '&lt;ReturnCoilTextAbs&gt;Q1.1&lt;/ReturnCoilTextAbs&gt; 13
+        lblReturnCoilAbs.Text = ParseRow(lstbxRowData.Items(13).ToString, "ReturnCoilTextAbs")
+
+        '&lt;ReturnDepthTextAbs&gt;I1.1&lt;/ReturnDepthTextAbs&gt; 14
+        lblReturnDepthAbs.Text = ParseRow(lstbxRowData.Items(14).ToString, "ReturnDepthTextAbs")
+
+        '&lt;ManSeq&gt;2&lt;/ManSeq&gt; 15
+        btnRet.Text = ParseRow(lstbxRowData.Items(15).ToString, "ManSeq")
+
+        '&lt;/Return&gt; 16
+        '&lt;Motion&gt; 17
+        '&lt;NameSym&gt;Clamp Cylinder&lt;/NameSym&gt; 18
+        lblMotionNameSym.Text = ParseRow(lstbxRowData.Items(18).ToString, "NameSym")
+
+        '&lt;NameAbs&gt;A1&lt;/NameAbs&gt; 19
+        lblMotionNameAbs.Text = ParseRow(lstbxRowData.Items(19).ToString, "NameAbs")
+
+
+
+        '&lt;/Motion&gt; 20
+        '&lt;/MotionRow&gt 21
+
 
 
     End Sub
 
+    Function ParseRow(ByVal InputRow As String, ByVal SearchString As String) As String
+
+        Dim startLocation As Integer
+
+        startLocation = InputRow.IndexOf(motionfooter)
+
+        Dim numLts As Integer
+        Dim numGts As Integer
+        Dim TextLocation As Integer
+        Dim TextLocation2 As Integer
+
+        Dim processString As String
+        Dim processString2 As String
+        Dim anotherint As Integer
 
 
+
+
+        '' MsgBox("InputRow: " & InputRow)
+
+        ''motionheader
+        ''motionfooter
+
+        ''  If InputRow.Contains(motionheader) Then
+        ''  MsgBox("it does contain numLts")
+        ''  End If
+
+
+
+        anotherint = UBound(Split(InputRow, SearchString))
+
+        If anotherint = 0 Then
+            '     MsgBox("no result")'error state
+        End If
+
+        If anotherint = 1 Then
+            'MsgBox("Only found one") 'error state
+        End If
+
+        If anotherint > 1 Then
+            'MsgBox("more than 1.." & anotherint) 'not error state
+        End If
+
+        ''MsgBox(anotherint)
+
+        ' processString = InputRow.Trim(vbCr)
+        ' processString = processString.Trim(vbLf)
+
+
+
+
+        TextLocation = InputRow.IndexOf(SearchString)
+        processString = InputRow.Substring(TextLocation + SearchString.Length)
+
+        TextLocation2 = processString.IndexOf(SearchString)
+        processString2 = processString.Remove(TextLocation2 - 1)
+
+        Dim finalheader As String = "&gt;"
+        Dim finalheaderlocation As Integer
+
+        finalheaderlocation = processString2.IndexOf(finalheader)
+        processString2 = processString2.Substring(finalheaderlocation)
+
+
+
+        Console.Write(processString2)
+
+        Console.Write(vbCrLf)
+        Return processString2
+
+
+
+
+    End Function
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles lblStnNumber.Click
+
+    End Sub
 End Class
